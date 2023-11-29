5c461f2432a1b9b73104bb59d789cfffce11d255
MudHolland <103423480+MudHolland@users.noreply.github.com>
2023-11-29 12:51:28 +0100

    set up docs file for typography

--- daden-inforgraphic-main-comp/.storybook/main.ts
+++ daden-inforgraphic-main-comp/.storybook/main.ts
@@ -1,18 +1,40 @@
-import type { StorybookConfig } from "@storybook/angular";
+// .storybook/main.ts
+
+import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
   ],
   framework: {
-    name: "@storybook/angular",
+    name: '@storybook/angular',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    extractArgTypes: false,
+    extractComponentDescription: (component, { notes }) => {
+      if (notes) {
+        return typeof notes === 'string' ? notes : notes.markdown || notes.text;
+      }
+      return null;
+    },
+    extractComponentSubtitle: (component, { parameters }) => {
+      return parameters && parameters.subtitle ? parameters.subtitle : null;
+    },
+    sidebar: {
+      Category: {
+        Components: ['src/*/*/*.mdx'],
+        // Add more categories and file patterns as needed
+      },
+    },
   },
 };
+
 export default config;
